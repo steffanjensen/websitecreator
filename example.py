2470c82d86baaa5e9b9ee68bdd8724cec591fbb9
reliefs <6hourapp@gmail.com>
2019-01-14 20:01:37 +0000

    fixing small bugs, added more comments

--- example.py
+++ example.py
@@ -1,8 +1,9 @@
+# Input user data for website
 title = input("Website title: ")
-websitetitle = input("Website title: ")
 keywords = input("Website keywords: ")
 metaDescription = input("Website meta description :")
 h1 = input("H1 text: ")
+article = input("Article text: ")
 filename = input("Output HTML Filename: ")
 
 
@@ -12,11 +13,18 @@ def htmloutput():
     f.write(
     f"""
     <html>
-    <body>
     <title>{title}<title>
     <meta name="keywords" content"{keywords}">
     <meta name="description" content="{metaDescription}"
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <body>
+    <main>
+	<header>{title}</header>
+	<nav>Nav bar...</nav>
+	<article>{article}</article>
+	<footer>Copyright 2019 - Yourdomain.com</footer>
+	</main>
     </body>
     </html>
     """
